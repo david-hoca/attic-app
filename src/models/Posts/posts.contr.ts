f5021151a24c2b10d7dd446a6b6da499a669d3c4
abduhamidbotirov <abduhamidbotirovwork@gmail.com>
2023-10-19 10:37:23 +0500

    pushed new repos

--- src/models/Posts/posts.contr.ts
+++ src/models/Posts/posts.contr.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import PostModel from './posts.model.js';
 import { uploadImg } from '../../utils/uploadImg.js';
 import handleError from '../../utils/catchError.js';
+import UserModel from '../User/user.model.js';
 class PostController {
     // Create a new post
     public createPost = async (req: Request, res: Response): Promise<void> => {
@@ -60,27 +61,47 @@ class PostController {
     public deletePost = async (req: Request, res: Response): Promise<void> => {
         // Implement the delete logic
     };
+    // Like a post
     public likePost = async (req: Request, res: Response): Promise<void> => {
         try {
             const { id } = req.params;
-            const userId = req.user.id; 
+            const userId = req.user.id; // Assuming user ID is available in the request
 
             const post = await PostModel.findById(id);
-
             if (!post) {
                 res.status(404).json({ error: 'Post not found' });
+                return;
+            }
+
+            const user = await UserModel.findById(userId);
+            if (!user) {
+                res.status(404).json({ error: 'User not found' });
                 return;
             }
 
             if (!post.likes.includes(userId)) {
                 post.likes.push(userId);
                 await post.save();
+                // Add the liked post to the user's likedPosts
+                user.likedPost.push(post._id);
+                await user.save();
+
+                res.status(200).json({ success: true });
+            } else {
+                // Remove the liked post from the user's likedPosts
+                user.likedPost = user.likedPost.filter((postId) => !postId.equals(post._id));
+                await user.save();
+
+                // Remove the user from the post's likes
+                post.likes = post.likes.filter((likedUserId) => !likedUserId.equals(userId));
+                await post.save();
+                res.status(200).json({ success: true, message: 'Post unliked' });
             }
-            res.status(200).json({ success: true });
         } catch (error: any) {
             handleError(res, error);
         }
     };
+
 }
 
 export default new PostController();
